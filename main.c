e810cea571011a7d06cf6fc59b9a1c7ed55bb58c
Hugo Damitzian <hdamitzi@student.42lyon.fr>
2023-01-26 10:19:44 +0100

    debut lsite chaine circulaire double

--- main.c
+++ main.c
@@ -1,32 +1,147 @@
-#include "libft/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 10:17:53 by hdamitzi          #+#    #+#             */
+/*   Updated: 2023/01/26 10:18:25 by hdamitzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	insert
+#include "push_swap.h"
 
-
-int	main(int ac, char **av)
+t_list	*init_new(int nb)
 {
-	int	*stack_a;
-	int	i;
-	int	j;
+	t_list	*new;
 
-	i = 1;
-	j = 0;
-	if (ac <= 2)
-	{
-		ft_printf("Il faut une liste d'entiers à trier");
+	new = malloc(sizeof(t_list));
+	if (!new)
 		exit(EXIT_FAILURE);
+	new->nb = nb;
+	new->next = new;
+	new->prev = new;
+}
+
+void	insert_begin(t_list **start, int nb)
+{
+	t_list	*last;
+	t_list	*new;
+
+	if (!(*start)->next && !(*start)->prev)
+	{
+		new = init_new(nb);
+		(*start) = new;
+		return ;
 	}
-	stack_a = malloc(sizeof(int) * (ac - 1));
-	if (!stack_a)
+	last = (*start)->prev;
+	new = malloc(sizeof(t_list));
+	if (!new)
 		exit(EXIT_FAILURE);
-	while (i < ac)
+	new->nb = nb;
+	new->next = (*start);
+	new->prev = last;
+	(*start)->prev = new;
+	last->next = new;
+	*start = new;
+}
+
+void	insert_back(t_list **start, int nb)
+{
+	t_list	*last;
+	t_list	*new;
+
+	if (!(*start)->next && !(*start)->prev)
 	{
-		stack_a[j] = ft_atoi(av[i]);
+		new = init_new(nb);
+		(*start) = new;
+		return ;
+	}
+	last = (*start)->prev;
+	new = init_new(nb);
+
+	//maintenant on insere le maillon dans la liste
+	//d'abord le next du last devient start
+	new->prev = last;
+
+	//maintenant le dernier doit savoir ou est le nouveau dernier
+	last->next = new;
+
+	//puis new doit savoir qui est le premier
+	new->next = (*start);
+
+	//start->prev pointait sur last il faut qu'il pointe sur new
+	(*start)->prev = new;
+}
+
+void	print_list(t_list **start)
+{
+	t_list	*last;
+	t_list	*current;
+
+	last = (*start)->prev;
+	current = (*start);
+	while (current->next != (*start))
+	{
+		printf("%d\n", current->nb);
+		current = current->next;
+	}
+	printf("%d\n", current->nb);
+}
+//le dernier devient premier
+void	reverse_rotate(t_list	**first, char pile)
+{
+	(*first) = (*first)->prev;
+}
+
+void	reverse(t_list **first, char pile)
+{
+	(*first) = (*first)->next;
+}
+
+void	swap(t_list **first, char pile)
+{
+	t_list	*temp;
+	t_list	*second;
+	t_list	*last;
+
+	temp = (*first);//temp est sur le premier maillon
+	second = temp->next;//second est le deuxieme
+	last = temp->prev;//le dernier de la chaine
+	(*first) = second;//first devient second
+	(*first)->prev = temp;//temp est first
+	temp->next = second->next;
+	second->next = temp;
+	second->prev = last;
+	last->next = second;
+}
+
+int main()
+{
+	t_list	*lst;
+
+	lst = malloc(sizeof(t_list));
+	if (!lst)
+		exit(EXIT_FAILURE);
+	lst->next = NULL;
+	lst->prev = NULL;
+
+	int		i = 0;
+	while (i < 4)
+	{
+		insert_begin(&lst, i);
 		i++;
-		j++;
 	}
-	for (int x = 0; x < ac - 1; x++)
-	{
-		ft_printf("%d\n", stack_a[x]);
-	}
+	print_list(&lst);
+	printf("\n");
+	reverse(&lst, 'a');
+	print_list(&lst);
+	printf("\n");
+	reverse_rotate(&lst, 'a');
+	print_list(&lst);
+	printf("\n");
+	swap(&lst, 'a');
+	print_list(&lst);
+	printf("\n");
 }
--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,14 @@
 /*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:12:38 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/01/26 10:12:16 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/01/26 10:18:56 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include "libft/libft.h"
 
 typedef struct s_list
 {
@@ -20,137 +21,3 @@ typedef struct s_list
 	struct s_list	*next;
 	struct s_list	*prev;
 }					t_list;
-
-t_list	*init_new(int nb)
-{
-	t_list	*new;
-
-	new = malloc(sizeof(t_list));
-	if (!new)
-		exit(EXIT_FAILURE);
-	new->nb = nb;
-	new->next = new;
-	new->prev = new;
-}
-
-void	insert_begin(t_list **start, int nb)
-{
-	t_list	*last;
-	t_list	*new;
-
-	if (!(*start)->next && !(*start)->prev)
-	{
-		new = init_new(nb);
-		(*start) = new;
-		return ;
-	}
-	last = (*start)->prev;
-	new = malloc(sizeof(t_list));
-	if (!new)
-		exit(EXIT_FAILURE);
-	new->nb = nb;
-	new->next = (*start);
-	new->prev = last;
-	(*start)->prev = new;
-	last->next = new;
-	*start = new;
-}
-
-void	insert_back(t_list **start, int nb)
-{
-	t_list	*last;
-	t_list	*new;
-
-	if (!(*start)->next && !(*start)->prev)
-	{
-		new = init_new(nb);
-		(*start) = new;
-		return ;
-	}
-	last = (*start)->prev;
-	new = init_new(nb);
-
-	//maintenant on insere le maillon dans la liste
-	//d'abord le next du last devient start
-	new->prev = last;
-
-	//maintenant le dernier doit savoir ou est le nouveau dernier
-	last->next = new;
-
-	//puis new doit savoir qui est le premier
-	new->next = (*start);
-
-	//start->prev pointait sur last il faut qu'il pointe sur new
-	(*start)->prev = new;
-}
-
-void	print_list(t_list **start)
-{
-	t_list	*last;
-	t_list	*current;
-
-	last = (*start)->prev;
-	current = (*start);
-	while (current->next != (*start))
-	{
-		printf("%d\n", current->nb);
-		current = current->next;
-	}
-	printf("%d\n", current->nb);
-}
-//le dernier devient premier
-void	reverse_rotate(t_list	**first, char pile)
-{
-	(*first) = (*first)->prev;
-}
-
-void	reverse(t_list **first, char pile)
-{
-	(*first) = (*first)->next;
-}
-
-void	swap(t_list **first, char pile)
-{
-	t_list	*temp;
-	t_list	*second;
-	t_list	*last;
-
-	temp = (*first);//temp est sur le premier maillon
-	second = temp->next;//second est le deuxieme
-	last = temp->prev;//le dernier de la chaine
-	(*first) = second;//first devient second
-	(*first)->prev = temp;//temp est first
-	temp->next = second->next;
-	second->next = temp;
-	second->prev = last;
-	last->next = second;
-}
-
-int main()
-{
-	t_list	*lst;
-
-	lst = malloc(sizeof(t_list));
-	if (!lst)
-		exit(EXIT_FAILURE);
-	lst->next = NULL;
-	lst->prev = NULL;
-
-	int		i = 0;
-	while (i < 4)
-	{
-		insert_begin(&lst, i);
-		i++;
-	}
-	print_list(&lst);
-	printf("\n");
-	reverse(&lst, 'a');
-	print_list(&lst);
-	printf("\n");
-	reverse_rotate(&lst, 'a');
-	print_list(&lst);
-	printf("\n");
-	swap(&lst, 'a');
-	print_list(&lst);
-	printf("\n");
-}
