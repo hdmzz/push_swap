932719117a6b09376e848af3d7b013486dd573db
Hugo Damitzian <hdamitzi@student.42lyon.fr>
2023-02-08 11:04:35 +0100

    debut algo index pour esquiver lint overflow

--- sort_utils.c
+++ sort_utils.c
@@ -6,11 +6,19 @@
 /*   By: hdamitzi <hdamitzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 11:23:07 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/02/08 08:30:25 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/02/08 10:43:58 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	cmp(int x, int y)
+{
+	int	diff;
+
+	diff = x - y;
+	return ( diff >> 31 ) | !!diff;
+}
 
 int	is_sorted(t_node *lst, int args_number)
 {
